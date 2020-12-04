a4c337f26c9acb0018aa723db8d1409508450527
Hao <pphenrique9@hotmail.com>
2020-12-04 12:45:31 -0300

    Update Primeira Lista Programação Aplicada IFPE (INCOMPLETO).py

--- Primeira Lista Programação Aplicada IFPE (INCOMPLETO).py
+++ Primeira Lista Programação Aplicada IFPE (INCOMPLETO).py
@@ -186,21 +186,21 @@ nota1 = float(input("29.1º) Digite a primeira nota: "))
 nota2 = float(input("29.2º) Digite a segunda nota: "))
 nota3 = float(input("29.3º) Digite a terceira nota: "))
 nota4 = float(input("29.4º) Digite a quarta nota: "))
-mediaAritmetica = (nota1+nota2+nota3+nota4)/4
+mediaAritmetica = (nota1+nota2+nota3+nota4) / 4
 print("A média aritmética é igual a: ",mediaAritmetica)
 
 
 #Atividade 30
 moedaReal = float(input("30.1º) Digite a quantia de dinheiro em reais: "))
 cotacaoDolar = float(input("30.2º) Digite o valor da cotação do dolar: "))
-moedaDolar = moedaReal*cotacaoDolar
+moedaDolar = moedaReal * cotacaoDolar
 print("Esse é o montante em dolares ",moedaDolar,"$")
 
 
 #Atividade 31
 numeroQualquer = int(input("31º) Digite um numero para saber qual é seu antecessor e seu sucessor: "))
-numeroAntecessor = numeroQualquer-1
-numeroSucessor = numeroQualquer+1
+numeroAntecessor = numeroQualquer - 1
+numeroSucessor = numeroQualquer + 1
 print("O antecessor e sucessor do numero digitado é",numeroAntecessor,numeroSucessor,"respectivivamente")
 
 
@@ -214,3 +214,156 @@ print("Essa é a soma do sucessor de seu triplo com o antecessor de seu dobro",c
 ladoQuadrado = float(input("33º) Digite o valor de um dos lados do quadrado: "))
 area = ladoQuadrado**2
 print("A área desse quadrado é de, ",area,"m²")
+
+
+#Atividade 34
+raioCirculo = float(input("34º) Digite o valor do raio do circulo: "))
+pi = 3.141592
+areaCirculo = pi * (raioCirculo**2)
+print("A área do circulo é igual a: ",areaCirculo)
+
+
+#Atividade 35
+catetoA = float(input("35.1º) Qual o valor do cateto oposto: "))
+catetoB = float(input("35.2º) Qual o valor do cateto adjacente: "))
+hipotenusa = ((catetoA**2) + (catetoB**2))**(1/2)
+print("Esse é o valor da hipotenusa: ",hipotenusa)
+
+
+#Atividade 36
+alturaCilindro = float(input("36.1º) Digite a altura do cilindro: "))
+raioCilindro = float(input("36.2º) Digite o raio do cilindro: "))
+pi = 3.141592
+volumeCilindro = pi * (raioCilindro**2) * alturaCilindro
+print("O volume do cilindro é de:", volumeCilindro)
+
+
+#Atividade 37
+valorDesconto = float(input("37º) Digite o valor do produto para saber quando fica com o desconto de 12%: "))
+valorFinal = valorDesconto * 0.88
+print("O valor final com desconto é de R$",valorFinal)
+
+
+#Atividade 38
+salario = float(input("38º) Digite o valor do seu salario: "))
+salarioNovo = salario * 1.25
+print("Seu salario com o aumento de 25% é igual a R$",salarioNovo)
+
+
+#Atividade 39
+print("39º) Iremos dividir o prêmio de 780.000,00 entre três ganhadores do concurso.")
+premioConcurso = 780.000
+primeiroLugar = premioConcurso * 0.54
+segundoLugar = premioConcurso * 0.68
+terceiroLugar = (primeiroLugar+segundoLugar)-premioConcurso
+print("O primeiro lugar fica com R$", primeiroLugar)
+print("O segundo lugar fica com R$", segundoLugar)
+print("O terceiro lugar fica com R$", terceiroLugar)
+
+
+#Atividade 40
+diariaEncanador = int(input("40º) Diga por quantos dias o encanador trabalhou? "))
+diaria = 30
+SalarioLiquido = diariaEncanador*diaria * 0.92
+print("O salario do encanador é de R$ ",SalarioLiquido)
+
+
+#Atividade 41
+horaPaga = float(input("Qual o valor em reais da sua hora trabalhada: "))
+horaTrabalhada = int(input("Digite quantas horas você trabalha pro dia: "))
+salarioPago = horaPaga*horaTrabalhada * 1.10
+print("O seu salario é de R$",salarioPago)
+
+
+#Atividade 42
+salarioBase = float(input("Informe o seu salario: "))
+salarioGratificado = salarioBase * 1.05
+impostoSalario = salarioGratificado * 0.93
+print("Seu salario com a gratificação de 5% mais o desconto de 7% de imposto é de R$",impostoSalario)
+
+
+#Atividade43
+valorProduto = float(input("Digite o valor do produto para saber as opções: "))
+valorAvista = valorProduto * 0.90
+valorParcelado = valorProduto / 3
+comissaoAvista = valorAvista * 0.05
+comissaoParcelado = (valorParcelado * 3) * 0.05
+print("O valor do produto avista fica de R$",valorAvista)
+print("O valor do produto parcelado fica 3 parcelas de R$", valorParcelado)
+print("Comissão do vendedor caso a venda seja avista é de R$", comissaoAvista)
+print("Comissao do vendedor caso a venda seja parcelada é de R$",comissaoParcelado)
+
+
+#Atividade 44
+alturaDegrau = float(input("Digite a altura do degrau em centimetros: "))
+alturaAlcancar = float(input("Digite a altura que deseja alcançar em centimetros: "))
+quantidadeDegrau = alturaDegrau/alturaAlcancar
+quantidadeInteira = int(quantidadeDegrau)
+print("Você deve subir",quantidadeInteira,"degraus")
+
+
+#Atividade 45
+numeroLido = int(input("Digite um numero inteiro de três digitos, de 100 a 999: "))
+unidadeNumero = numeroLido // 1 % 10
+dezenaNumero = numeroLido // 10 % 10
+centenaNumero = numeroLido // 100 % 10
+print("A ordem inversa dos numeros é:", unidadeNumero,dezenaNumero,centenaNumero)
+
+
+#Atividade 46
+numeroLido = int(input("Digite um numero inteiro de quatro digitos, de 1000 a 9999: "))
+unidadeNumero = numeroLido // 1 % 10
+dezenaNumero = numeroLido // 10 % 10
+centenaNumero = numeroLido // 100 % 10
+milharNumero = numeroLido // 1000 % 10
+print("Primeiro numero:",milharNumero)
+print("Segundo numero:",centenaNumero)
+print("Terceiro numero:",dezenaNumero)
+print("Quarto numero:",unidadeNumero)
+
+
+#Atividade 47
+segundos = int(input("Digite a quantidade de segundos a ser convertida: "))
+horas = segundos // 3600
+segundosRestante = segundos % 3600
+minutos = segundosRestante // 60
+segundosRestante = segundosRestante % 60
+print(horas,"horas",minutos,"minutos",segundosRestante,"segundos")
+
+
+#Atividade 48 *** OBSERVAÇÃO ***
+horaInicio = int(input("Diga que horas são valores de 1 a 12:"))
+minutoInicio = int(input("Diga quantos minutos tem a hora valores de 0 a 59: "))
+segundoInicio = int(input("Diga quantos segundos tem a hora valores de 0 a 60: "))
+duracaoEventoBiologico = 7200
+horas = duracaoEventoBiologico / 3600
+minutos = (duracaoEventoBiologico-(horas*3600)) // 60
+segundos = duracaoEventoBiologico - (horas*3600) - (minutos*60)
+horaFinal = horaInicio + horas
+minutoFinal = minutoInicio + minutos
+segundoFinal = segundoInicio + segundos
+print("O evento termina às", int(horaFinal),"horas", int(minutoFinal), "minutos", int(segundoFinal),"segundos")
+
+
+#Atividade 49
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
